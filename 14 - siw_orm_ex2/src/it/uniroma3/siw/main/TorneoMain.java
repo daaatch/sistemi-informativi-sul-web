f1dc08f4aecd226e908d0539171026389532a06c
datch <vlad.turno@gmail.com>
2025-07-18 04:13:31 +0200

    Add files via upload

---
+++ 14 - siw_orm_ex2/src/it/uniroma3/siw/main/TorneoMain.java
@@ -0,0 +1,19 @@
+package it.uniroma3.siw.main;
+
+import javax.persistence.*;
+
+public class TorneoMain {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("torneo-unit");
+		EntityManager em = emf.createEntityManager();
+
+		System.out.println("Hello World!");
+		
+		em.close();
+		emf.close();
+
+	}
+	
+}
---
+++ 14 - siw_orm_ex2/test/it/uniroma3/siw/model/TorneoTest.java
@@ -0,0 +1,18 @@
+package it.uniroma3.siw.model;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+import java.util.List;
+import javax.persistence.*;
+import org.junit.jupiter.api.*;
+
+class TorneoTest {
+
+	@Test
+	public void shouldAssertTrue() {
+		assertTrue(1==1);
+	}
+	
+}
