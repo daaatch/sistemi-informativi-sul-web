b2eda6dd2c25e6d4a033b26e23018be918f58a5f
datch <vlad.turno@gmail.com>
2025-07-18 04:00:44 +0200

    Add files via upload

---
+++ 05 - siw_jpa_ex5/src/it/uniroma3/siw/model/Customer.java
@@ -0,0 +1,132 @@
+package it.uniroma3.siw.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@NamedQuery(name = "findAllCustomers", query = "SELECT c FROM Customer c")
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"firstName", "lastName", "email"}))
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String firstName;
+	
+	@Column(nullable = false)
+	private String lastName;
+	
+	private String email;
+	
+	private String phoneNumber;
+	
+	@Column(nullable = false)
+	private LocalDate dateOfBirth;
+	
+	@Column(nullable = false)
+	private LocalDate registrationDate;
+	
+	@OneToOne(fetch = FetchType.EAGER)
+	private Address address;
+	
+	@OneToMany(mappedBy = "customer", fetch = FetchType.LAZY)
+	@OrderBy("creationTime asc")
+	private List<Order> orders;
+	
+	public Customer() {
+	}
+	
+	public Customer(String firstName, String lastName, LocalDate dateOfBirth, LocalDate registrationDate, Address address) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dateOfBirth = dateOfBirth;
+		this.registrationDate = registrationDate;
+		this.address = address;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public LocalDate getRegistrationDate() {
+		return registrationDate;
+	}
+
+	public void setRegistrationDate(LocalDate registrationDate) {
+		this.registrationDate = registrationDate;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dateOfBirth, firstName, lastName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		return Objects.equals(dateOfBirth, other.dateOfBirth) && Objects.equals(firstName, other.firstName)
+				&& Objects.equals(lastName, other.lastName);
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", phoneNumber=" + phoneNumber + ", dateOfBirth=" + dateOfBirth + ", registrationDate="
+				+ registrationDate + ", address=" + address + "]";
+	}
+	
+}
