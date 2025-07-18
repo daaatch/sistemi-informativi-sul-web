aa3ce24d403c3472a5c34165bb7d02cf8f895b9c
datch <vlad.turno@gmail.com>
2025-07-18 03:59:44 +0200

    Add files via upload

---
+++ 04 - siw_jpa_ex4/src/it/uniroma3/siw/model/OrderLine.java
@@ -0,0 +1,73 @@
+package it.uniroma3.siw.model;
+
+import java.util.Objects;
+
+import javax.persistence.*;
+
+@Entity
+@NamedQuery(name = "findAllOrderLines", query = "SELECT ol FROM OrderLine ol")
+public class OrderLine {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	private Float unitPrice;
+	
+	@Column(nullable = false)
+	private int quantity;
+	
+	@ManyToOne
+	private Order order;
+	
+	@ManyToOne
+	private Product product;
+	
+	public OrderLine() {
+	}
+	
+	public OrderLine(Float unitPrice, int quantity) {
+		this.unitPrice = unitPrice;
+		this.quantity = quantity;
+	}
+
+	public Float getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(Float unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, quantity, unitPrice);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderLine other = (OrderLine) obj;
+		return Objects.equals(id, other.id) && quantity == other.quantity && Objects.equals(unitPrice, other.unitPrice);
+	}
+
+	@Override
+	public String toString() {
+		return "OrderLine [id=" + id + ", unitPrice=" + unitPrice + ", quantity=" + quantity + "]";
+	}
+
+}
---
+++ 04 - siw_jpa_ex4/src/it/uniroma3/siw/model/Provider.java
@@ -0,0 +1,87 @@
+package it.uniroma3.siw.model;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.*;
+
+@Entity
+public class Provider {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	private String phoneNumber;
+	
+	private String email;
+	
+	@Column(unique = true, nullable = false)
+	private String partitaIva;
+	
+	@ManyToMany
+	private List<Product> products;
+	
+	@OneToOne
+	private Address address;
+	
+	public Provider() {
+	}
+	
+	public Provider(String name, String phoneNumber, String email) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, name, phoneNumber);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Provider other = (Provider) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(name, other.name)
+				&& Objects.equals(phoneNumber, other.phoneNumber);
+	}
+
+	@Override
+	public String toString() {
+		return "Provider [id=" + id + ", name=" + name + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
+	}
+	
+}
